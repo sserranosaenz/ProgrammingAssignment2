2a8fc41b702c0541c36fb9f8c62593b2d37698a2
sserranosaenz <sonny_sss@hotmail.com>
2015-05-19 17:59:17 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
